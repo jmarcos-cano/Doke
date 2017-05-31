9727d83d275f5520485458f49fdffa2b92b7f373
Marcos Cano <marcoscano@MarcosMac.local>
2017-05-31 14:56:53 -0600

    Adds Mac BUILD_DATE & GIT_BRANCH support

--- .Makefile.settings.mk
+++ .Makefile.settings.mk
@@ -1,14 +1,13 @@
 SHELL=/bin/bash -e -o pipefail
 
-include .env
-
-
-export NEXUS_URL?=http://nexus.dev.autoweb.com/repository/
 # Docker build-args
 export GIT_REPO ?= $(shell git config --get remote.origin.url )
+export GIT_BRANCH ?= $(shell git branch|cut -d" " -f 2 )
+
 export GIT_COMMIT ?= $(shell git rev-parse --short HEAD)
 export GIT_COMMIT_DETAILS ?= $(shell git log -1 --pretty=oneline )
-export BUILD_DATE := $(shell date --rfc-3339=seconds )
+#export BUILD_DATE := $(shell date --rfc-3339=seconds )
+export BUILD_DATE :=$(shell date "+%Y-%m-%d %H:%M:%S")
 export BUILD_TAG ?= $(shell hostname)
 
 
@@ -21,11 +20,14 @@ NC := "\e[0m"
 INFO := @bash -c 'printf $(YELLOW); printf "=> $$1 \n"; printf $(NC)' MESSAGE
 WARNING := @bash -c 'printf $(RED); printf "WARNING: $$1 \n"; printf $(NC)' MESSAGE
 
+## Meta
 metadata_file=.metadata
-metadata= "Library was created  \n\
+metadata= "Docker image was created  \n\
+      IMAGE: $(IMAGE):$(VERSION) \n\
       BUILD_DATE: $(BUILD_DATE) \n\
       GIT_REPO: $(GIT_REPO) \n\
       GIT_COMMIT: $(GIT_COMMIT) \n\
+      GIT_BRANCH: $(GIT_BRANCH)  \n\
       BUILD_TAG: $(BUILD_TAG) \n\
       "
 
