bb4e79134083ed0f9f4b990bb23acee10dd90dfa
Marcos Cano <marcoscano@MarcosMac.local>
2017-06-09 08:23:37 -0600

    Adds installation scripts

---
+++ installation/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+DOWNLOAD_URL=https://raw.githubusercontent.com/jmarcos-cano/Doke/master/downloads
+TAR_FILE=doke.tar.gz
+
+DEPENDENCIES='curl tar'
+
+for DEP in ${DEPENDENCIES};do
+	if ! location="$(type -p "${DEP}")" || [ -z "$location" ]; then
+	  echo "Missing dependency: ${DEP}, please make sure to install it"
+	  exit 1
+	fi   
+done
+
+
+curl -k -s "${DOWNLOAD_URL}/${TAR_FILE}" | tar -xvz
