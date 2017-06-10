5f05bf4755880045dac6b7cd65f1471c43e9dd6b
Marcos Cano <marcoscano@MarcosMac.local>
2017-06-10 00:03:46 -0600

    Adds init target in .makefile

--- doke-src/.Makefile.settings.mk
+++ doke-src/.Makefile.settings.mk
@@ -1,5 +1,9 @@
 SHELL=/bin/bash -e -o pipefail
 
+
+
+.DEFAULT_GOAL := all
+
 # Docker build-args
 export GIT_REPO ?= $(shell git config --get remote.origin.url )
 export GIT_BRANCH ?= $(shell git branch|cut -d" " -f 2 )
@@ -11,7 +15,6 @@ export BUILD_DATE :=$(shell date "+%Y-%m-%d %H:%M:%S")
 export BUILD_TAG ?= $(shell hostname)
 
 
-.DEFAULT_GOAL := all
 
 # Cosmetics
 RED := "\e[1;31m"
@@ -31,9 +34,39 @@ metadata= "Docker image was created  \n\
       BUILD_TAG: $(BUILD_TAG) \n\
       "
 
+## default values
+_BASE_IMAGE= alpine:3.6
+_SRC_CODE_DIR= .
+_VERSION= v0.0.0
+_IMAGE= $(shell basename $(shell pwd)|sed 's/[^a-z  A-Z]//g')
+
+dot_env_fle= " \n\
+\#\# Compile time vars \n\
+BASE_IMAGE=$(_BASE_IMAGE) \n\
+OUTPUT_DIR=output \n\
+SRC_CODE_DIR=$(_SRC_CODE_DIR) \n\
+BUILD_COMMAND=\"pwd && ls\"  \n\
+\#\# Doker image vars  \n\
+IMAGE=$(_IMAGE) \n\
+VERSION=$(_VERSION) \n"
+
+
+
 label: ## prints out label-schema metadata
 	@ ${INFO} ${metadata}
 	@printf ${metadata} > ${metadata_file}
+
+
+
+init: ## generates .env content
+	@ ${INFO} "Generating .env file according to your project"
+	@read  -p "What BASE_IMAGE will you like to compile your code with? [defaults to: $(_BASE_IMAGE)] " _BASE_IMAGE
+	@read  -p "Where is your source code located? (SRC_CODE_DIR) [defaults to: $(_SRC_CODE_DIR)] " _SRC_CODE_DIR
+	@read  -p "What would you like to name your generated Docker image? (IMAGE) [defaults to: $(_IMAGE)] " _IMAGE
+	@read  -p "What is the version you'll like to use? (VERSION) [defaults to: $(_VERSION)" _VERSION
+	@ ${INFO} "Copy and paste this in your .env file"
+	@printf ${dot_env_fle} 
+
 
 
 help: ## prints this help message
